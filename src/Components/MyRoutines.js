9ffe8368dfea625021ffbf71047cc4d65f731222
Rebecca Carr <reca9238@colorado.edu>
2021-11-27 12:40:30 -0700

    worked on My Routines and Routines

--- src/Components/MyRoutines.js
+++ src/Components/MyRoutines.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { getUser, getRoutinesByUser } from "../apiCalls";
+import { getUser, getRoutinesByUser, getActivities } from "../apiCalls";
 
 const NewRoutine = ({token}) => {
     return <>
@@ -25,18 +25,118 @@ const NewRoutine = ({token}) => {
         </>
 }
 
+const DeleteRoutine = ({token}) => {
+    return  <button onClick = {(event) => { deleteRoutine(token, routine.id, setRoutines)}}> Delete Routine</button>
+}
+
+const EditRoutine = ({token}) => {
+    return <form onSubmit = {(event) => {
+        event.preventDefault();
+        editRoutine(token, routine.id, name, goal, setRoutines);
+
+    }}>
+    <input 
+        placeholder='Name*'
+        onChange={(event) => setName(event.target.value)} />    
+    <input 
+        placeholder='Goal*'
+        onChange={(event) => setGoal(event.target.value)} />
+        <input type='submit' />
+    </form>
+}
+
+const AddActivity = ({activities}) => {
+    return <form onSubmit = {(event) => {
+        event.preventDefault();
+        console.log(routine.id, activityId, count, duration);
+        attachActivity(routine.id, activityId, count, duration, setRoutines);
+    }}>
+        <select  onChange = {(event) => {setActivityId(event.target.value)}}>
+            <option value = {"none"}> Select an activity: </option>
+            {activities.map((activity, key) => {
+                return <option key = {key} value = {activity.id} >{activity.name}</option>
+            })}
+        </select>
+        <input 
+            placeholder='Count*'
+            onChange={(event) => setCount(event.target.value)} />
+         <input 
+            placeholder= 'Duration*'
+            onChange={(event) => setDuration(event.target.value)} />
+        <input type = 'submit' />
+    </form>
+}
+
+const Activity = ({activity}) => {
+    return <>
+    <p><b> {activity.name} </b></p>
+    <p><b>Description: </b>{activity.description}</p>
+    <p><b>Duration: </b>{activity.duration}</p>
+    <p><b>Count: </b>{activity.count}</p>
+    </>
+}
+
+const DeleteActivity = ({token, activity}) => {
+    return <button onClick = { () => {
+        console.log(activity.routineActivityId);
+        deleteRoutineActivity(token, activity.routineActivityId, setRoutines);
+    }}>Remove Activity</button>
+}
+
+const EditRoutineActivity = ({token, activity}) => {
+    return <form onSubmit = {(event) => {
+        event.preventDefault();
+        editRoutineActivity(token, activity.routineActivityId, count, duration, setRoutines);
+    }}>
+    <input 
+        placeholder='Count*'
+        onChange={(event) => setCount(event.target.value)} />
+    <input 
+        placeholder= 'Duration*'
+        onChange={(event) => setDuration(event.target.value)} />
+    <input type = 'submit' />
+    </form>
+}
+
 const MyRoutines = ({token}) => {
     const [myRoutines, setMyRoutines] = useState([]);
-    const [user, setUser] = useState({});
+    const [activities, setActivities] = useState([]);
 
     useEffect(async () => {
-        // getUser(token, setUser);
+        const user = await getUser(token);
+        setMyRoutines(await getRoutinesByUser(user.username, token));
+        setActivities(await getActivities());
+   
         
 
     }, [])
     return(<>
     <h1>My Routines</h1>
     <NewRoutine  token = {token}/>
+    {myRoutines.map((routine, key) => {
+        return(<div key = {key}>
+            <h4><b>{routine.name}</b></h4>
+            <p><b>Goal: </b>{routine.goal}</p>
+            <p><b>Creator: </b>{routine.creatorName}</p>
+            <DeleteRoutine token = {token}/>
+            <h3>Edit Routine</h3>
+            <EditRoutine token = {token} />
+            <h3>Add Activity to Routine</h3>
+            <AddActivity activities = {activities}/>
+            <p><b>Activities: </b></p>
+            {routine.activities.map((activity, key) => {
+                return <div key = {key} >
+                    <Activity activity = {activity} />
+                    <DeleteActivity token = {token} activity = {activity}/>
+                    <p><b>Edit Routine Activity</b></p>
+                    <EditRoutineActivity token = {token} activity = {activity} />
+                    </div>
+            })}
+        </div>
+        
+          
+        )
+    })}
 
     </>)
 
--- src/Components/Routines.js
+++ src/Components/Routines.js
@@ -1,31 +1,16 @@
 import React, {useState, useEffect} from "react";
-import { getRoutines, 
-        postRoutine, 
-        getUser, 
-        deleteRoutine, 
-        editRoutine,
-        getActivities,
-        attachActivity,
-        deleteRoutineActivity,
-        editRoutineActivity } from "../apiCalls";
+import { getRoutines } from "../apiCalls";
+
 
 const Routines = ({token}) => {
     const [routines, setRoutines] = useState([]);
-    const [name, setName] = useState("");
-    const [goal, setGoal] = useState("");
-    const [user, setUser] = useState({});
-    const [activities, setActivities] = useState([]);
-    const [activityId, setActivityId] = useState();
-    const [count, setCount] = useState("");
-    const [duration, setDuration] = useState("");
     
 
     useEffect(async () => {
-        getRoutines(setRoutines);
-        getUser(token, setUser);
-        setActivities(await getActivities());
+        setRoutines(await getRoutines());
 
     }, [])
+
     
     return (
     <>
@@ -33,74 +18,11 @@ const Routines = ({token}) => {
         {routines.map((routine, key) => {return(
         <div key = {key}>
             <h4><b>{routine.name}</b></h4>
-            {user.id === routine.creatorId? <> 
-                <button onClick = {(event) => { deleteRoutine(token, routine.id, setRoutines)}}> Delete Routine</button>
-                <h3>Edit Routine</h3>
-                <form onSubmit = {(event) => {
-                    event.preventDefault();
-                    editRoutine(token, routine.id, name, goal, setRoutines);
-
-                }}>
-                <input 
-                    placeholder='Name*'
-                    // value={name}
-                    onChange={(event) => setName(event.target.value)} />
-            <input 
-                    placeholder='Goal*'
-                    // value={goal}
-                    onChange={(event) => setGoal(event.target.value)} />
-                    <input type='submit' />
-                </form>
-                <h3>Add Activity to Routine</h3>
-                <form onSubmit = {(event) => {
-                    event.preventDefault();
-                    console.log(routine.id, activityId, count, duration);
-                    attachActivity(routine.id, activityId, count, duration, setRoutines);
-                }}>
-                    <select  onChange = {(event) => {setActivityId(event.target.value)}}>
-                        <option value = {"none"}> Select an activity: </option>
-                        {activities.map((activity, key) => {
-                            return <option key = {key} value = {activity.id} >{activity.name}</option>
-                        })}
-                    </select>
-                    <input 
-                        placeholder='Count*'
-                        // value={count}
-                        onChange={(event) => setCount(event.target.value)} />
-                     <input 
-                        placeholder= 'Duration*'
-                        // value={duration}
-                        onChange={(event) => setDuration(event.target.value)} />
-                    <input type = 'submit' />
-                </form>
-
-            </>: null}
             <p><b>Goal: </b>{routine.goal}</p>
             <p><b>Creator: </b>{routine.creatorName}</p>
             <div><b>Activities: </b>{routine.activities.map((activity, key) => {
                 return <div key = {key}>
                     <p><b>{activity.name}</b></p>
-                    {user.id === routine.creatorId? <>
-                        <button onClick = { () => {
-                            console.log(activity.routineActivityId);
-                            deleteRoutineActivity(token, activity.routineActivityId, setRoutines);
-                        }}>Remove Activity</button>
-                        <h3>Edit Routine Activity</h3>
-                        <form onSubmit = {(event) => {
-                            event.preventDefault();
-                            editRoutineActivity(token, activity.routineActivityId, count, duration, setRoutines);
-                        }}>
-                        <input 
-                            placeholder='Count*'
-                            // value={count}
-                            onChange={(event) => setCount(event.target.value)} />
-                        <input 
-                            placeholder= 'Duration*'
-                            // value={duration}
-                            onChange={(event) => setDuration(event.target.value)} />
-                        <input type = 'submit' />
-                        </form>
-                    </>: null}
                     <p><b>Description: </b>{activity.description}</p>
                     <p><b>Duration: </b>{activity.duration}</p>
                     <p><b>Count: </b>{activity.count}</p>
