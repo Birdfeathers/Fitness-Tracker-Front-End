00b3a166c6b5263d6a966e76667a6daae4e8703c
JordisonMatthew <jordison.m@hotmail.com>
2021-11-27 12:26:09 -0700

    home should be finished

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from "react";
+import { getUser } from "../apiCalls";
 
 const Home = ({token}) => {
+    const [username, setUsername] = useState('');
+
+    useEffect(async () => {
+        const result = await getUser(token);
+
+        setUsername(result.username);
+    }, [token])
     return (
-        <h1>HOME</h1>
+        <h1>Welcome {username? username : 'Stranger'}</h1>
     )
 }
 
